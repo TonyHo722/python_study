b29bcaf94aadefa5358081435d34d067a19ee51c
tonyho <tonyho@via.com.tw>
2024-03-12 20:41:38 +0800

    update file

--- create_file/create_file.py
+++ create_file/create_file.py
@@ -2,7 +2,7 @@
 # create output file
 writefile = open("test.txt", "w")
 line_number = 100
-for num in range(1, 10000):
+for num in range(1, 20000):
     print(num)
     writefile.write(f"number = {num}\n")
 
